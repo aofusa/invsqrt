985842ae008bef70231eebe590ab642ef57524a2
Aofusa <5000enPCinAOFUSA@gmail.com>
2024-09-27 16:24:31 +0000

    remove unsafe

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,17 @@
 
 pub fn invsqrt(x: f32) -> f32 {
-    unsafe {
-        // y = 1/sqrt(x) = 2 ^ (-1/2 * log2(x))
-        let dx: i32 = *(&x as *const f32 as *const i32);
-        let dy: i32 = 0x5F3759DF - (dx >> 1);  // Magic number
-        let mut y: f32 = *(&dy as *const i32 as *const f32);
+    // y = 1/sqrt(x) = 2 ^ (-1/2 * log2(x))
+    let dx = x.to_bits();
+    let dy = 0x5F3759DF - (dx >> 1);  // Magic number
+    let mut y = f32::from_bits(dy);
 
-        // Newton's method
-        let threehalfs = 1.5f32;
-        let x2 = x * 0.5f32;
-        y = y * (threehalfs - (x2 * y * y));  // 1st iteration
-        y = y * (threehalfs - (x2 * y * y));  // 2nd iteration
+    // Newton's method
+    let threehalfs = 1.5f32;
+    let x2 = x * 0.5f32;
+    y = y * (threehalfs - (x2 * y * y));  // 1st iteration
+    y = y * (threehalfs - (x2 * y * y));  // 2nd iteration
 
-        y
-    }
+    y
 }
 
 #[cfg(test)]
@@ -21,13 +19,11 @@ mod tests {
     use super::*;
 
     fn fabs(x: f32) -> f32 {
-        unsafe {
-            let dx: i32 = *(&x as *const f32 as *const i32);
-            let dy: i32 = 0x7FFFFFFF & dx;  // flip first bit (sign bit)
-            let y: f32 = *(&dy as *const i32 as *const f32);
+        let dx = x.to_bits();
+        let dy = 0x7FFFFFFF & dx;  // flip first bit (sign bit)
+        let y = f32::from_bits(dy);
 
-            y
-        }
+        y
     }
 
     #[test]
